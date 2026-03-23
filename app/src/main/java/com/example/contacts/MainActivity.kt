ccff2c98106fc6da6c1b706c1d0b945c83d4acb7
munir <munironi1997@gmail.com>
2026-03-23 19:19:12 +0500

    initdsf

--- app/src/main/java/com/example/contacts/MainActivity.kt
+++ app/src/main/java/com/example/contacts/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             ContactsTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Anaaadroid",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
